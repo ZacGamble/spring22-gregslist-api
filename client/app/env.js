0e65c2dc1c01edf0c91bb4e5eb87b5d83f87f067
Zac Gamble <zacrgamble@gmail.com>
2022-04-19 13:53:22 -0600

    first

--- client/app/env.js
+++
@@ -1,5 +0,0 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = 'dev-wglcqf-n.us.auth0.com'
-export const audience = 'https://MarkDev.com'
-export const clientId = 'La0Mv5rjSAdmXsSbXD7g8jUUfVLpdIwM'
